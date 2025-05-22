28dba6652ee6a80d671ff692967f0b9314c5bd10
Tafadzwa <193275436+TmuD-05@users.noreply.github.com>
2025-05-22 07:14:35 +0200

    Add header component with logo and title styling
    Created a reusable Header component using React
    Positioned logo image and title text side-by-side using Flexbox
    Aligned header content to the center of the page
    Applied consistent spacing and basic styling to match the design

---
+++ vite-project/src/assets/components/header.jsx
@@ -0,0 +1,12 @@
+import './header.css';
+export default function Header(){
+    return(
+     <main >
+        <nav className='header-body'>
+             <img src="/cropped_image.png" className='nav-img'/>
+            <p> my travel journal </p>
+        </nav>
+    </main>
+    )
+   
+}
